5e86d9d1e4fd39df9f94856941cbba3e065ac3dd
Zygimantas <zygis@skip.build>
2025-03-13 13:29:41 +0100

    fix: use temporal config

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"context"
+	"flag"
 	"github.com/skip-mev/ironbird/app"
 	"github.com/skip-mev/ironbird/types"
+)
+
+var (
+	configFlag = flag.String("config", "./conf/app.yaml", "Path to the app configuration file")
 )
 
 func main() {
 	ctx := context.Background()
 
-	cfg, err := types.ParseAppConfig("./conf/app.yaml")
+	cfg, err := types.ParseAppConfig(*configFlag)
 
 	if err != nil {
 		panic(err)
