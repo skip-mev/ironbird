b28efc647bedc5dba3d9d066ca9dcfd6ddbc764c
Zygimantas <zygis@skip.build>
2025-05-10 14:05:47 +0200

    feat: add loadbalancer infrastructure

--- types/testnet/types.go
+++ types/testnet/types.go
@@ -2,6 +2,7 @@ package testnet
 
 type RunnerType string
 
+// TODO: move this out into messages
 const (
 	DigitalOcean RunnerType = "DigitalOcean"
 	Docker       RunnerType = "Docker"
@@ -9,6 +10,7 @@ const (
 
 type Node struct {
 	Name    string
+	Address string
 	Rpc     string
 	Lcd     string
 	Metrics string
