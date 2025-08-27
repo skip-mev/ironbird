3ae9b6b203265b5e288067aafaa5a000755555ab
Tyler <48813565+technicallyty@users.noreply.github.com>
2025-08-27 11:13:40 -0700

    go back to latest

--- activities/loadtest/loadtest.go
+++ activities/loadtest/loadtest.go
@@ -151,7 +151,7 @@ func (a *Activity) RunLoadTest(ctx context.Context, req messages.RunLoadTestRequ
 	task, err := p.CreateTask(ctx, provider.TaskDefinition{
 		Name: "catalyst",
 		Image: provider.ImageDefinition{
-			Image: "ghcr.io/skip-mev/catalyst:0.0.0-beta.9",
+			Image: "ghcr.io/skip-mev/catalyst:latest",
 			UID:   "100",
 			GID:   "100",
 		},
